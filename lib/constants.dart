df57f9e503517aaa65ba3cfdad168c2d18bd04f3
MarcelliaNr <marcellianr1234@gmail.com>
2023-02-09 12:09:30 +0700

    update warna

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 // list of colors that we use in our app
-const kBackgroundColor = Color.fromARGB(255, 199, 195, 186);
-const kPrimaryColor = Color.fromARGB(255, 38, 147, 241);
+const kBackgroundColor = Color.fromARGB(255, 114, 223, 223);
+const kPrimaryColor = Color.fromARGB(255, 219, 204, 182);
 const kSecondaryColor = Color.fromARGB(255, 230, 135, 71);
 const kTextColor = Color(0xFF000839);
 const kTextLightColor = Color(0xFF747474);
